222211626158d426b1a62dad63ecc2c5520e46cb
Uros <urosspasenovic9@gmail.com>
2022-01-17 20:52:57 +0100

    html and css for login

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <title>Pretplata</title>
+
+  </head>
+  <body>
+  <div class="container">
+  <div class="container">
+  <form method="GET" action="#"> 
+    <div class="left">
+      <div class="header">
+        <h2 class="animation a1">Welcome Back</h2>
+        <h4 class="animation a2">Log in to your account using username and password</h4>
+      </div>
+      <div class="form">
+      <input type="text" placeholder="username" name="username" class="form-field animation a3"  required>
+        <input type="password" placeholder="password" name="password" class="form-field animation a4" required>
+        <button type="submit" class="login-button" name="submit">Log in</button> 
+      </div>
+    </div>
+    <div class="right"></div>
+    </form>
+  </div>
+  <body>
+  </html>
